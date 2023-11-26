73f2966e405600cd138b64a4734f762b7b31a040
hogumachu <castle_jun@daum.net>
2023-11-26 19:05:49 +0900

    chore: NavigationView 추가

--- DesignKit/Package.swift
+++ DesignKit/Package.swift
@@ -5,15 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "DesignKit",
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "DesignKit",
             targets: ["DesignKit"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.6.0"),
+    ],
     targets: [
         .target(
-            name: "DesignKit"
+            name: "DesignKit",
+            dependencies: [
+                "RxSwift",
+                .product(name: "RxCocoa", package: "RxSwift")
+            ]
         ),
     ]
 )
