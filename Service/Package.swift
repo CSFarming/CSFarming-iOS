dfabe7d6d0af95d06164083ff8aeba562423b15b
hogumachu <castle_jun@daum.net>
2023-12-03 18:28:40 +0900

    project: iOS Target 변경 (16->17)

--- Service/Package.swift
+++ Service/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Service",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "BaseService",
