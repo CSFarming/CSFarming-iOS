509d8afd0b9d5204542bcd87870afbaa086072e0
hogumachu <castle_jun@daum.net>
2023-12-03 21:50:35 +0900

    chore: HomeAPI 제거

--- Service/Sources/HomeService/HomeService.swift
+++ Service/Sources/HomeService/HomeService.swift
@@ -10,43 +10,14 @@ import Moya
 import RxSwift
 import BaseService
 
-public protocol HomeServiceInterface: HomeVisitServiceInterface {
-    
-    func requestElements() -> Single<[ContentElement]>
-    func requestElements(path: String) -> Single<[ContentElement]>
-    func requestElementsWithPrefix(path: String) -> Single<[ContentElement]>
-    
-}
+public protocol HomeServiceInterface: HomeVisitServiceInterface {}
 
 public final class HomeService: HomeServiceInterface {
     
-    private let provider: MoyaProvider<HomeAPI>
     private let repository: HomeRepositoryInterface
-    private let parser: HomeParserInterface
     
-    public init(
-        provider: MoyaProvider<HomeAPI> = .init(),
-        repository: HomeRepositoryInterface,
-        parser: HomeParserInterface
-    ) {
-        self.provider = provider
+    public init(repository: HomeRepositoryInterface) {
         self.repository = repository
-        self.parser = parser
-    }
-    
-    public func requestElements() -> Single<[ContentElement]> {
-        return provider.request(.list)
-            .map(parser.parse)
-    }
-    
-    public func requestElements(path: String) -> Single<[ContentElement]> {
-        let request: Single<HomeListResponse> = provider.request(.detail(path: path))
-        return request.map { $0.toElements() }
-    }
-    
-    public func requestElementsWithPrefix(path: String) -> Single<[ContentElement]> {
-        let request: Single<HomeListResponse> = provider.request(.detailWithPrefix(path: path))
-        return request.map { $0.toElements() }
     }
     
     public func requestVisitHistory() -> Single<[ContentElement]> {
